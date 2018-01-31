cb2a42d324fd10ed979cfc3c62da0706eec6f0ab
alicdn <35830283+alicdn@users.noreply.github.com>
2018-02-01 05:00:19 +0800

    Create yum.sh

---
+++ yum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#一键更换私人yum源
+cd /etc/yum.repos.d
+mv CentOS-Base.repo CentOS-Base.repo.backup
+mv epel.repo epel.repo.backup
+mv epel-testing.repo epel-testing.repo.backup
+wget 12345bt.github.io/CentOS-Base.repo
+wget 12345bt.github.io/epel.repo
+yum clean metadata
+yum makecache
+yum -y upgrade
+
+#重启服务器
+shutdown -r now
