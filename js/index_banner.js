0b4ea5171e957f2b56fcde4caa8a9e8c8c2f9406
alicdn <35830283+alicdn@users.noreply.github.com>
2018-02-04 19:00:04 +0800

    Create index_banner.js

---
+++ js/index_banner.js
@@ -0,0 +1,5 @@
+document.writeln("<div class=\'dbm_banner\'>");
+document.writeln("<a href=\'http://www.isoo.mobi\' target=\'_blank\' rel=\'external nofollow\'><img src=\'http://wx4.sinaimg.cn/large/006D4Va5gy1fhzz70lz0ag30rs01ogmq.gif\' alt=\'\' title=\'\'></a>");  //5716
+document.writeln("<a href=\'http://www.isoo.mobi\' target=\'_blank\' rel=\'external nofollow\'><img src=\'http://wx4.sinaimg.cn/large/006D4Va5gy1fhzz70lz0ag30rs01ogmq.gif\' alt=\'\' title=\'\'></a>");  //5716
+document.writeln("<a href=\'http://www.isoo.mobi\' target=\'_blank\' rel=\'external nofollow\'><img src=\'http://wx4.sinaimg.cn/large/006D4Va5gy1fhzz70lz0ag30rs01ogmq.gif\' alt=\'\' title=\'\'></a>");  //5716
+document.writeln("</div>");
