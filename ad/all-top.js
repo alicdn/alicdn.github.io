467db9e78382d1f8f3725ed1f554b188c51206c3
alicdn <35830283+alicdn@users.noreply.github.com>
2018-05-11 16:55:09 +0800

    Update all-top.js

--- ad/all-top.js
+++ ad/all-top.js
@@ -1,6 +1,5 @@
 document.writeln("<div align=\'center\' style=\'margin:5px 0 5px 20px;\'>");
 document.writeln("<a href=\'https://349603.cc/sese/153dy.html\' target=\'_blank\'><img name=\'ad\' src=\'https://i.loli.net/2018/05/11/5af53f8d19742.gif\' width=\'100%\' height=\'70\' /></a>");
-document.writeln("<A href=\'http://www.9279927.com:88/376845.html\' target=\'_blank\'><img border=\'0\' src=\'http://wx4.sinaimg.cn/large/006JjYT1gy1fdps9l1wikg30qo02847k.gif\' width=\'100%\' height=\'60\'></A>");
-document.writeln("<A href=\'http://www.8014445.com:88/13072.html\' target=\'_blank\'><img border=\'0\' src=\'https://wx3.sinaimg.cn/mw1024/006V1cijgy1fq8xbqfe08g30qo028anw.gif\' width=\'100%\' height=\'60\'></A>");
-
+document.writeln("<A href=\'http://www.9279927.com:88/376845.html\' target=\'_blank\'><img border=\'0\' src=\'/ad/9927.gif\' width=\'100%\' height=\'60\'></A>");
+document.writeln("<A href=\'http://www.8014445.com:88/13072.html\' target=\'_blank\'><img border=\'0\' src=\'/ad/4445.gif\' width=\'100%\' height=\'60\'></A>");
 document.writeln("</div>");
