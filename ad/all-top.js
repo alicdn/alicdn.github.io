ae1567b125d8c84f9c93a3994642ba08cec350d7
alicdn <35830283+alicdn@users.noreply.github.com>
2018-05-11 14:54:46 +0800

    Update all-top.js

--- ad/all-top.js
+++ ad/all-top.js
@@ -1,4 +1,3 @@
 document.writeln("<div align=\'center\' style=\'margin:5px 0 5px 20px;\'>");
 document.writeln("<a href=\'https://349603.cc/sese/153dy.html\' target=\'_blank\'><img name=\'ad\' src=\'http://www.sedh.xyz/images/hengfu1.gif\' width=\'80%\' height=\'60\' /></a>");
-document.writeln("<a href=\'https://349603.cc/sese/153dy.html\' target=\'_blank\'><img name=\'ad\' src=\'http://www.sedh.xyz/images/hengfu1.gif\' width=\'80%\' height=\'60\' /></a>");
 document.writeln("</div>");
