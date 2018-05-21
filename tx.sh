226326d11a15be56531553ecc0ed09bb635f90cb
alicdn <35830283+alicdn@users.noreply.github.com>
2018-05-21 09:52:14 +0800

    Update tx.sh

--- tx.sh
+++ tx.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-/usr/local/qcloud/stargate/admin/uninstall.sh
-/usr/local/qcloud/YunJing/uninst.sh
-/usr/local/qcloud/monitor/barad/admin/uninstall.sh
+#fuck tx process
+rm -rf /usr/local/sa
+rm -rf /usr/local/agenttools
+rm -rf /usr/local/qcloud
+process=(sap100 secu-tcs-agent sgagent64 barad_agent agent agentPlugInD pvdriver )
+for i in ${process[@]}
+do
+  for A in $(ps aux | grep $i | grep -v grep | awk '{print $2}')
+  do
+    kill -9 $A
+  done
+done
+chkconfig --level 35 postfix off
+service postfix stop
+echo ''>/var/spool/cron/root
