07fb8fc7af663e61669c87e7346dc7104e167e2a
alicdn <35830283+alicdn@users.noreply.github.com>
2019-05-31 15:45:09 +0800

    Update head.js

--- css/head.js
+++ css/head.js
@@ -14,15 +14,26 @@ document.writeln("<span class=\"red f14\"><font color=\"red\">亲，你知道吗
 
 }
 
+
+//logo
+function tongji(){
+
+document.writeln("<div class=\'bobologo mb15\'><a href=\'/\'><img src=\'https://alicdn.github.io/logo/btlogo.png\' alt=\'Magnetic Search BT Seed Search Tool Free download BT seed, over 40 million seed data\' /></a></div>");
+
+}
+
+
 //全站底部
 function foot(){
+
 document.writeln("<p><a href=\'/new.html\' title=\'最新100部 \' target=\'_blank\'>最新100部</a>&nbsp;<a href=\/\/\'/tag-1.html\' title=\/\/\'热门搜索 \' target=\'_blank\'>热门搜索</a>&nbsp;<a href=\'/nvyou-1.html\' title=\'人气明星 \' target=\'_blank\'>人气明星</a>&nbsp;<a href=\'/map/sitemap.xml\' title=\'地图索引 \' target=\'_blank\'>地图索引</a></p>");
 
 }
 
+
 //统计
 function tongji(){
-	
+
 document.writeln("<p style=\'display: none;\'><script type=\'text/javascript\' src=\'//js.users.51.la/5300013.js\'></script></p>");
 
 }
