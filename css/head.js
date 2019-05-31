4c42092b20d3fda9152aaa54b847f462d6d381ee
alicdn <35830283+alicdn@users.noreply.github.com>
2019-05-31 15:39:06 +0800

    Update head.js

--- css/head.js
+++ css/head.js
@@ -1,12 +1,21 @@
 
+
+//投诉提示
 function tousu(){
 
 document.writeln("<span class=\"grey f12\"><p>本站不存储任何资源内容，只收集BT种子元数据(例如文件名和文件大小)和磁力链接(BT种子标识符)，并提供查询服务，是一个完全合法的搜索引擎系统。 网站不提供种子下载服务，用户可以通过第三方链接或磁力链接获取到相关的种子资源。本站也不对BT种子真实性及合法性负责，请用户注意甄别！投诉邮箱：tousu@"+ document.domain.replace(/^(m.|www.)/g,'') +"</p></span>");
 
 }
 
+//提示
 function qin(){
 
 document.writeln("<span class=\"red f14\"><font color=\"red\">亲，你知道吗？下载的人越多速度越快，赶快把本页面分享给好友一起下载吧^_^ </font></span><br>");
+
+}
+
+//全站底部
+function foot(){
+document.writeln("<p><a href=\'/new.html\' title=\'最新100部 \' target=\'_blank\'>最新100部</a>&nbsp;<a href=\/\/\'/tag-1.html\' title=\/\/\'热门搜索 \' target=\'_blank\'>热门搜索</a>&nbsp;<a href=\'/nvyou-1.html\' title=\'人气明星 \' target=\'_blank\'>人气明星</a>&nbsp;<a href=\'/map/sitemap.xml\' title=\'地图索引 \' target=\'_blank\'>地图索引</a></p>");
 
 }
