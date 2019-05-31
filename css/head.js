1bd56f3cc09c503a76d452284be98f5c051edcb4
alicdn <35830283+alicdn@users.noreply.github.com>
2019-05-31 15:32:39 +0800

    Create head.js

---
+++ css/head.js
@@ -0,0 +1,12 @@
+
+function tousu(){
+
+document.writeln("<span class=\"red f14\"><p>本站不存储任何资源内容，只收集BT种子元数据(例如文件名和文件大小)和磁力链接(BT种子标识符)，并提供查询服务，是一个完全合法的搜索引擎系统。 网站不提供种子下载服务，用户可以通过第三方链接或磁力链接获取到相关的种子资源。本站也不对BT种子真实性及合法性负责，请用户注意甄别！投诉邮箱：tousu@"+ document.domain.replace(/^(m.|www.)/g,'') +"</p></span>");
+
+}
+
+function qin(){
+
+document.writeln("<span class=\"red f14\"><font color=\"red\">亲，你知道吗？下载的人越多速度越快，赶快把本页面分享给好友一起下载吧^_^ </font></span><br>");
+
+}
