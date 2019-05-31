31bc88848bc888f636558c18ca1ba63ca186bc5c
alicdn <35830283+alicdn@users.noreply.github.com>
2019-05-31 15:42:48 +0800

    Update head.js

--- css/head.js
+++ css/head.js
@@ -19,3 +19,10 @@ function foot(){
 document.writeln("<p><a href=\'/new.html\' title=\'最新100部 \' target=\'_blank\'>最新100部</a>&nbsp;<a href=\/\/\'/tag-1.html\' title=\/\/\'热门搜索 \' target=\'_blank\'>热门搜索</a>&nbsp;<a href=\'/nvyou-1.html\' title=\'人气明星 \' target=\'_blank\'>人气明星</a>&nbsp;<a href=\'/map/sitemap.xml\' title=\'地图索引 \' target=\'_blank\'>地图索引</a></p>");
 
 }
+
+//统计
+function tongji(){
+	
+document.writeln("<p style=\'display: none;\'><script type=\'text/javascript\' src=\'//js.users.51.la/5300013.js\'></script></p>");
+
+}
